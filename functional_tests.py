ae059b6219a2b9b0316a7ed9c24fb6208f394c63
Ulrike Port <uport@byteyard.de>
2016-01-28 16:28:36 +0000

    write template to separate file

--- functional_tests.py
+++ functional_tests.py
@@ -17,17 +17,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # and indeed, the site exists
         self.assertIn('To-Do', self.browser.title) 
-        self.fail('Finsih the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # There is an input field asking for a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+                )
 
         # She inserts her first to-do item and press enter
+        inputbox.send_keys('buy milk')
 
         # the item is added to her list
+        inputbox.send_keys(Keys.ENTER)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: buy milk' for row in rows)
+                )
         # entering a second item extends the list
 
         # the next day, she checks the website again wondering
         # whether her list was stored. Happily, the list is still 
         # available
+        self.fail('Finsih the test!')
 
 if __name__ == '__main__':  #7
     unittest.main()  #8
